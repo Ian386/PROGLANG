8eca5b77ac4951584eb1f44f82cf644476bb9d4c
Ian Ndolo Mwau <134711923+Ian386@users.noreply.github.com>
2023-09-29 11:26:05 +0300

    prac1.py

--- prac1.py
+++ prac1.py
@@ -1,23 +1,13 @@
-#name - IAN NDOLO MWAU 
+#NAME- IAN NDOLO MWAU
 #REG NO - SCT211-0034/2022
+
 # A simple calculator to add two numbers
 
-def calc(a, b):
-    var = input("Enter the operation: ")
-    if var == "a":
-        print(a+b)
-    elif var == "s":
-        print(a-b)
-    elif var == "m":
-        print(a*b)
-    elif var == "d":
-        print(a/b)
-    else:
-        print("invalid operation")
-        
-    
+str1 = input("Enter your first name: ")
+print("Welcome", str1, "to my basic calculator of adding two numbers")
 var1 = int(input("Enter the first number: "))
 var2 = int(input("Enter the second number: "))
-calc(var1, var2)
+result = var1 + var2
+print("The sum of", var1,"and", var2, "is:", result)
 
 
